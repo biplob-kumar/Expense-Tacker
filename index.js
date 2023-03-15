c23cd7fb76be803a5422b367d0be7142bb2de09c
biplob kumar <biplobkumar125@gmail.com>
2023-03-15 08:47:16 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const blance =document.getElementById("blance");
+const mony_plus =document.getElementById("mony-plus");
+const mony_minus =document.getElementById("mony-minus");
+const list =document.getElementById("list");
+const form =document.getElementById("form");
+const text =document.getElementById("text");
+const Number =document.getElementById("Number");
+
+
+const dummyTransactions = [
+  { id: 1, text: "Flower", amount: -20 },
+  { id: 2, text: "Salary", amount: 300 },
+  { id: 3, text: "Book", amount: -10 },
+  { id: 4, text: "Camera", amount: 150 },
+];
